dd20ea3a607da6ab68e86b587a699563920dd76b
Eric Petsopoulos <epetsopoulos@gmail.com>
2022-10-21 03:05:25 +0100

    new prompt seeds

--- init.py
+++ init.py
@@ -13,7 +13,7 @@ def generate_prompt():
     generator = pipeline('text-generation', model="Gustavosta/MagicPrompt-Stable-Diffusion")
     # set_seed(42)
     # create a few different prompt seeds and then serve a random one to the prompt-generator
-    prompt_seeds=['amazing architecture in a', 'beautiful landscape of', 'an inspirational view of nature', 'impressive complex natural structures in the', 'profound view of nature', 'impossible view of nature', 'satisfying vistas of the astral dimension', 'grand buildings set in a', 'massive byzantine church in a', 'huge complex buildings from a', 'colourful natural scene set in a', 'beautiful natural geometry in a forest', 'a mysterious forest with huge trees', 'a magical wild forest with', 'beautiful view of the sky', 'heavenly clouds', 'the radiant sun', 'healing view of nature']
+    prompt_seeds=['amazing architecture in a', 'beautiful landscape of', 'an inspirational view of nature', 'impressive complex natural structures in the', 'impossible view of nature', 'grand buildings set in a', 'massive byzantine church in a', 'huge complex buildings from a', 'colourful natural scene set in a', 'beautiful natural geometry in a forest', 'a mysterious forest with huge trees', 'a magical wild forest with', 'beautiful view of the sky', 'inside a huge complex building', 'dream like view of a', 'psychedelic wonderland in the forest', 'huge plane in the sky', 'giant cave with a forest', 'mega architecture in the sky', 'inspirational architecture in a', 'beautiful spiritual realm', 'stagering mountains in the', 'impressive trees', 'snow tundra', 'the sun shining', 'geometric clouds', 'lush clouds', 'green grass lawns in the suburbs', 'imaginary forest', 'cliffs and the sea', 'huge cave opening to a city', 'big skies in heaven', 'the sea with waves', 'peaceful lake ripples', 'vast lake scotland', 'forest in a anlen planet', 'peaceful pond in a garden with', 'botanical garden with huge greenhouse in a', 'a magical garden with lots of flowers', 'beautiful ancient city with trees', 'huge cathedral in a', 'latge mosque in a', 'lovely little village peaceful beautiful', 'fire campsite forest pine trees', 'beautiful reflective water lake in a park', 'golden summer in a beautful park', 'winter wonderland in the', 'october in a west sussex forest', 'huge beautiful complex water fountain', 'huge waterfall in the', 'big river floating on clouds', 'valley of mirrors river of wind', 'beautiful country side with animals in the', 'a huge castle in scotland', 'huge clouds in  heaven']
     prompt = generator(random.choice(prompt_seeds), max_length=90, num_return_sequences=1)
     prompt = prompt[0]['generated_text']
     print(f"ℹ️ prompt: {prompt}")
@@ -21,11 +21,13 @@ def generate_prompt():
 
 # * step 2) feed prompt into image-to-text model
 def generate_image(prompt):
+    # 'CompVis/stable-diffusion-v1-4'
+    # 'johnslegers/stable-diffusion-v1-5'
     model_id = "CompVis/stable-diffusion-v1-4"
     num_inference_steps = 50 # default = 50
     guidance_scale = 7.5 # default = 7.5
-    image_height = 768 # 512 # 1024
-    image_width = 512 # 256 # 768
+    image_height = 512 # 768 # 1024
+    image_width = 512 # 512 # 768
     image_name = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
 
     pipe = StableDiffusionPipeline.from_pretrained(model_id, use_auth_token=True)
@@ -36,10 +38,13 @@ def generate_image(prompt):
 
     # * upscale image
     def upscale_image(image_name, image_path):
-        model = RealESRGAN(torch.device('cpu'), scale=8)
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        model = RealESRGAN(device, scale=4)
         model.load_weights('weights/RealESRGAN_x4.pth', download=True)
         image = Image.open(image_path).convert('RGB')
+        print('upscaling image x4...')
         upscaled_image = model.predict(image)
+        print('✅')
         upscaled_image_path = f"./images/{image_name}_4x.png"
         upscaled_image.save(upscaled_image_path) 
 
