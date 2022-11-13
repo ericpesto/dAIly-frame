1cf604ede5eeb3c7d8187e09c063c34958b0061d
Eric Petsopoulos <epetsopoulos@gmail.com>
2022-11-13 16:01:43 +0000

    preprush

--- generate_image.py
+++ generate_image.py
@@ -17,7 +17,7 @@ from RealESRGAN import RealESRGAN
 # * step 1) use text-generator model to create prompt
 def generate_prompt():
     generator = pipeline('text-generation', model="Gustavosta/MagicPrompt-Stable-Diffusion")
-    prompt_seeds = ['highly detailed matte oil painting of english countryside, heavenly, vivid, at', 'highly detailed matte oil painting of beautiful organic house in a forest, complex, detailed, vivid, heavenly, at', 'highly detailed matte oil painting of english forest, heavenly, vivid, at', 'highly detailed matte oil painting of elegant english countryside estate, heavenly, vivid', 'highly detailed matte oil painting of english countryside village, heavenly, vivid']
+    prompt_seeds = ['highly detailed matte oil painting of english countryside, heavenly, vivid, at', 'highly detailed matte oil painting of english forest, heavenly, vivid, at', 'highly detailed matte oil painting of elegant english countryside estate, heavenly, vivid', 'beautiful architecture in']
     prompt = generator(random.choice(prompt_seeds), max_length=200, num_return_sequences=1)
     prompt = prompt[0]['generated_text']
     print(f"ℹ️ prompt: {prompt}")
@@ -26,7 +26,7 @@ def generate_prompt():
 # * step 2) feed prompt into image-to-text model
 def generate_image(prompt):
     model_id = "CompVis/stable-diffusion-v1-4"
-    num_inference_steps = 75 # default = 50, sweetspot = 75, max = 100
+    num_inference_steps = 100 # default = 50, sweetspot = 75, max = 100
     guidance_scale = 7.5 # default = 7.5
     image_height = 768 # 1024
     image_width = 512 # 768
@@ -39,7 +39,7 @@ def generate_image(prompt):
     print('done ✅')
 
     def upscale_image(image):
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        device = torch.device('cpu')
         model = RealESRGAN(device, scale=4)
         model.load_weights(f"weights/RealESRGAN_x4.pth", download=True)
         print('upscaling image x4... ⏳')
@@ -52,6 +52,6 @@ def generate_image(prompt):
     image.save(image_path)
     print(f"image saved ✅") 
 
-for _ in range(5):
+for _ in range(1):
     image_prompt = generate_prompt()
     generate_image(image_prompt)
